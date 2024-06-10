51fa622244383f4265963ccf4781fd92f950a386
Mohamed Sorour <mohammedyasser827@gmail.com>
2024-06-10 18:25:08 +0300

    Setup and initialize all bases

---
+++ frontend/dynamocards/src/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const [responseData, setResponseData] = useState(null);
+
+  const handleLinkChange = (event) => {
+    setYoutubeLink(event.target.value);
+  };
+
+  const sendLink = async() => {
+    try{
+      const response = await axios.post("http://localhost:8000/analyze_video", {
+        youtube_link: youtubeLink,
+      });
+      setResponseData(response.data);
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className='App'>
+      <h1>YouTube Link to Flashcards Generator</h1>
+      <input
+        type='text'
+        placeholder='Paste YouTube Link Here'
+        value={youtubeLink}
+        onChange={handleLinkChange}
+      />
+      <button onClick={sendLink}>
+        Generate Flashcards
+      </button>
+      {responseData && (
+        <div>
+          <h2>Response Data: </h2>
+          <p>{JSON.stringify(responseData, null, 2)}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App; 
