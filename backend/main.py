dd00e0473a6539108477b0bf119bc7ada0086b2b
Mohamed Sorour <mohammedyasser827@gmail.com>
2024-06-12 01:02:00 +0300

    Abstraction

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
 
+from services.genai import YoutubeProcessor
+
 class VideoAnalysisRequest(BaseModel):
     youtube_link: HttpUrl
 
@@ -19,25 +21,10 @@ app.add_middleware(
 @app.post("/analyze_video")
 def analyze_video(request: VideoAnalysisRequest):
 
-    from langchain_community.document_loaders import YoutubeLoader
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
-    docs = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    result = text_splitter.split_documents(docs)
-
-
-    author=result[0].metadata['author']
-    length=result[0].metadata['length']
-    title=result[0].metadata['title']
-    total_size=len(result)
+    processor = YoutubeProcessor()
+    result= processor.retrieve_youtube_documents(str(request.youtube_link))
 
     return{
-        "author": author,
-        "length": length,
-        "title": title,
-        "total_size": total_size,
+        "result": result
     }
 
